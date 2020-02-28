cddc5238f1438917c1e0559cf07508391827f814
Zwakele <zwakele.mgabhi@standardbank.co.za>
2020-02-28 06:07:05 +0200

    28/02: annotations @Value & java config

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -1,10 +1,24 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan("com.luv2code.springdemo")
+//@ComponentScan("com.luv2code.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 
+    // define bean for sad fortune service
+    @Bean
+    public FortuneService sadFortuneService() {
+        return new SadFortuneService();
+    }
+
+    // define bean for swim coach and inject
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach(sadFortuneService());
+    }
 }
---
+++ src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -0,0 +1,22 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SwimJavaConfigDemoApp {
+
+	public static void main(String[] args) {
+		
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
+		SwimCoach coach = context.getBean("swimCoach", SwimCoach.class);
+		
+		System.out.println(coach.getDailyWorkout());
+		System.out.println(coach.getDailyFortune());
+		System.out.println("email: " + coach.getEmail());
+		System.out.println("team: " + coach.getTeam());
+
+		context.close();
+
+	}
+
+}
